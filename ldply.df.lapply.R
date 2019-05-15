f227101a2348163ead1891fd8576767561d30e97
Jake Saunders <jakesaunders@gmail.com>
2019-05-15 14:54:02 -0400

    Update ldply.df.lapply.R

--- ldply.df.lapply.R
+++ ldply.df.lapply.R
@@ -3,3 +3,7 @@ test <- ldply (
     lbls,
     function(x) { chemosen[chemosen$pID == x, ]}
 )
+
+#https://stackoverflow.com/questions/26177565/converting-nested-list-to-dataframe 
+library(data.table)
+rbindlist(mylist, fill=TRUE) 
