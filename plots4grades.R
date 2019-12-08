0382ba2d6abcfef1f507bcc95632a99421f0096a
Jake Saunders <jakesaunders@gmail.com>
2019-12-07 23:57:10 -0500

    Add files via upload

--- plots4grades.R
+++ plots4grades.R
@@ -1,34 +1,70 @@
-# Load grades 
-df <- read.csv("grades-final.csv")
-ea <- df$Finalgrades.num
-ea <- ea[!is.na(ea)]
+# histogram for grades
+histGrades <- function(
+    grades,
+    lims = c(110,94.0,90.0,86.7,83.4,80.0,76.7,73.4,70.0,66.7,63.4,60.0,0),
+    maj.lims = c(90,80,70,60,0),
+    xrange = c(50,100),
+    bar.col = "gold",
+    med.lty = 4,
+    med.col = "green",
+    med.lwd = 5,
+    mean.lty = 4,
+    mean.col = "purple",
+    mean.lwd = 5,
+    ...
+){
+    grades <- na.omit(grades)
+    lims <- sort(lims)
+    hi <- hist(grades, breaks = lims, freq = T, xlim=xrange, col=bar.col, ...)
+    
+    # median line
+    abline(v = median(grades,T), lty=med.lty,col=scales::alpha(med.col,0.4),lw=med.lwd)
+    text(x = median(grades),y = 0.5,pos = 1,labels = "median",cex = .75)
+    # mean line
+    abline(v = mean(grades), lty=mean.lty,col=scales::alpha(mean.col,0.4),lw=mean.lwd)
+    text(x = mean(grades),y = 0.25,pos = 1,labels = "mean",cex = .75)
+    
+    # add lines for grades
+    abline(v = maj.lims,lty=3,lwd=4,col=c("dimgrey"))
+    
+    # add grades
+    counts <- hist(grades,breaks = c(110,maj.lims),plot = F)
+    text(x = c(95,85,75,65,55),y = max(hi$counts),labels = c("A","B","C","D","F"))
+    text(x = c(55,65,75,85,95),y = max(hi$counts)-1,labels = counts$counts)
+}
 
-# grade breaks
-lims <- sort(c(100,94.0,90.0,86.7,83.4,80.0,76.7,73.4,70.0,66.7,63.4,60.0,59.9,0))
+# histogram for grades
+boxGrades <- function(
+    grades,
+    lims = c(94.0,90.0,86.7,83.4,80.0,76.7,73.4,70.0,66.7,63.4,60.0),
+    maj.lims = c(90,80,70,60),
+    yrange = c(40,100),
+    box.col = "gold",
+    notch=TRUE,
+    frame=FALSE,
+    dot.col="blue",
+    dot.cex=1,
+    med.pch = 15,
+    med.col = "green",
+    mean.pch = 18,
+    mean.col = "purple",
+    ABCDF=c(95,85,75,65,55),
+    ...
+){
+    grades <- na.omit(grades)
+    
+    boxplot(grades, col = box.col,ylim=yrange, notch=notch, frame=FALSE,...)
+    
+    points(median(ea),pch=med.pch,cex=2,col=scales::alpha(med.col,0.4))
+    points(mean(grades),pch=mean.pch,cex=2,col=scales::alpha(mean.col,0.4))
+    legend("topright",legend = c("median","mean"),bty="n", pch=c(med.pch,mean.pch),
+           text.col = c(med.col,mean.col),col=c(med.col,mean.col), cex=1.5)
+    
+    text(x = .5,y=ABCDF,labels = c("A","B","C","D","F"))
+    abline(h = lims,lty=3,lwd=2,col="azure3")
+    abline(h = maj.lims,lty=3,lwd=4,col="dimgrey")
+    
+    stripchart(grades,col = scales::alpha(dot.col,0.4),pch=19,method = "jitter",
+               vertical = T,add=T,cex=dot.cex)
+}
 
-# get center coords for histogram
-tb <- hist(ea,breaks = c(110,90.0,80.0,70.0,60.0,0),freq = T,ylim=c(0,20))$counts
-
-
-par(mfrow=c(1,2))
-hist(ea,breaks = lims,freq = T,xlim=c(40,110),col="gold",
-     main = "BIO114 Final Grade 2019 Spring")
-#abline(v = lims,lty=3,lwd=4,col=c("lightgrey"))
-abline(v = median(ea,na.rm = T), lty=4,col=scales::alpha("green",0.4),lw=5)
-abline(v = mean(ea,na.rm = T), lty=4,col=scales::alpha("purple",0.4),lw=5)
-abline(v = c(90,80,70,60),lty=3,lwd=4,col=c("dimgrey"))
-text(x = mean(ea,na.rm = T),y = 0.3,labels = "mean",cex = .75)
-text(x = median(ea,na.rm = T),y = 0.5,labels = "median",cex = .75)
-text(x = c(95,85,75,65,55),y = 10,labels = c("A","B","C","D","F"))
-text(x = c(55,65,75,85,95),y = 9,labels = tb)
-
-boxplot(ea, notch = T,col = "gold",ylim=c(50,100), main="BIO114 Final Grade 2019 Spring")
-abline(h = c(90,80,70,60),lty=3,lwd=4,col=c("dimgrey"))
-abline(h = c(94.0,86.7,83.4,76.7,73.4,66.7,63.4,59.9),lty=3,lwd=4,col=c("azure3"))
-points(mean(ea,na.rm = T),pch=10,cex=2,col="purple")
-text(x = mean(ea,na.rm = T),labels = "mean",cex = .75,col="purple")
-stripchart(ea,col = scales::alpha("blue",0.4),pch=19,method = "jitter",
-           vertical = T,add=T,cex=1)
-text(x = .5,y=c(95,85,75,65,55),labels = c("A","B","C","D","F"))
-text(x = .6,y=c(55,65,75,85,95),labels = tb)
-par(mfrow=c(1,1))
